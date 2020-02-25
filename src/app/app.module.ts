f279f21859b8020b421715da68f6905916ab03f1
ANihalani007 <akshay.nihalani@microfocus.com>
2020-02-25 15:37:24 +0530

    2 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { MediaItemComponent } from './media-item/media-item.component';
 
 
 
 
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, MediaItemComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
