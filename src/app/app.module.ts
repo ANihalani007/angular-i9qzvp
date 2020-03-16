0bead86786db23acdc1c90b3dedeb3065ff62d58
ANihalani007 <akshay.nihalani@microfocus.com>
2020-03-16 12:31:35 +0530

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { EventsAppComponent } from './events-app.component';
-import { MediaItemComponent } from './media-item/media-item.component';
+
 
 
 
 
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ EventsAppComponent, MediaItemComponent ],
+  declarations: [ EventsAppComponent ],
+  providers:[],
   bootstrap:    [ EventsAppComponent ]
 })
 export class AppModule { }
